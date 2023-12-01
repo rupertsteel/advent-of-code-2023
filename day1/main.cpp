77c4a7400649839a32ae7a2c3c8ad7f0c09ac273
Rupert Steel <rupert@rupertsteel.me>
2023-12-01 21:12:52 +1300

    Day 1 part 2

--- day1/main.cpp
+++ day1/main.cpp
@@ -25,7 +25,40 @@ int main(int argc, char* argv[]) {
 	auto processed1 = std::string_view{ input } | std::views::split("\n"sv) | std::views::transform([](auto rng) { return std::string_view(rng.begin(), rng.end()); });
 	auto processed2 = processed1 | std::views::filter([](auto sv) { return !sv.empty(); });
 
-	auto processed3 = processed2 | std::views::transform([](auto rng1) { return rng1 | std::views::filter([](auto d) { return std::isdigit(d); }); });
+	auto processed3 = processed2 | std::views::transform([](std::string_view sv) {
+		std::vector<char> returnVec;
+
+		while (!sv.empty()) {
+			if (sv.starts_with("one")) {
+				returnVec.push_back('1');
+			} else if (sv.starts_with("two")) {
+				returnVec.push_back('2');
+			} else if (sv.starts_with("three")) {
+				returnVec.push_back('3');
+			} else if (sv.starts_with("four")) {
+				returnVec.push_back('4');
+			} else if (sv.starts_with("five")) {
+				returnVec.push_back('5');
+			} else if (sv.starts_with("six")) {
+				returnVec.push_back('6');
+			} else if (sv.starts_with("seven")) {
+				returnVec.push_back('7');
+			} else if (sv.starts_with("eight")) {
+				returnVec.push_back('8');
+			} else if (sv.starts_with("nine")) {
+				returnVec.push_back('9');
+			} else {
+				if (isdigit(sv[0])) {
+					returnVec.push_back(sv[0]);
+				}
+			}
+
+			sv = sv.substr(1);
+		}
+
+		//return rng1 | std::views::filter([](auto d) { return std::isdigit(d); });
+		return returnVec;
+	});
 
 	auto processed4 = processed3 | std::views::transform([](auto rng) { return (rng | std::views::take(1) | std::ranges::to<std::string>() ) + (rng | std::views::reverse | std::views::take(1) | std::ranges::to<std::string>()); });
 
