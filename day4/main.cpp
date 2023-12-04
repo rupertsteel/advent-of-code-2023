e04fc95315d91c10e2c46c3b85d97636f7fda1d4
Rupert Steel <rupert@rupertsteel.me>
2023-12-04 18:47:04 +1300

    Day 4 part 2

--- day4/main.cpp
+++ day4/main.cpp
@@ -67,13 +67,36 @@ Card stringViewToCard(std::string_view sv) {
 	return card;
 }
 
-int cardToScore(const Card& card) {
+int numMatching(const Card& card) {
 
 	std::vector<int> outputSet;
 
 	auto overlap = std::ranges::set_intersection(card.winingPoints, card.havePoints, std::back_inserter(outputSet));
 
-	return std::pow(2, outputSet.size() - 1);
+	return outputSet.size();
+}
+
+int cardToScore(const Card& card) {
+
+	int matches = numMatching(card);
+
+	if (matches == 0) {
+		return 0;
+	}
+
+	return std::pow(2, matches - 1);
+}
+
+std::pair<int, std::string_view> stringViewToCardNumStringView(std::string_view sv) {
+	auto workingSv = sv;
+
+	workingSv.remove_prefix(4);
+
+	int numEnd = workingSv.find(':');
+
+	int gameNum = std::stoi(std::string{ workingSv.begin(), workingSv.begin() + numEnd });
+
+	return std::make_pair(gameNum, sv);
 }
 
 int main(int argc, char* argv[]) {
@@ -92,13 +115,42 @@ int main(int argc, char* argv[]) {
 
 	auto totalScore = std::accumulate(scoreView.begin(), scoreView.end(), 0);
 
+	auto cardNumCardView =
+		processed2 | std::views::transform(stringViewToCardNumStringView) |
+		std::views::transform([](std::pair<int, std::string_view> val) {
+			return std::make_pair(val.first, stringViewToCard(val.second));
+		})
+		| std::views::transform([](std::pair<int, Card> val) {
+			return std::make_pair(val.first, numMatching(val.second));
+		});
+	std::map<int, int> gameResults = cardNumCardView | std::ranges::to<std::map<int, int>>();
+
+	std::map<int, int> numCard;
+	for (const auto& card : gameResults) {
+		numCard[card.first] = 1;
+	}
+
+	int numCardsProcessed = 0;
+	for (int i = 1; i <= numCard.size(); i++) {
+		auto sourceCards = numCard.at(i);
+
+		numCardsProcessed += sourceCards;
+
+		auto numToPushTo = gameResults.at(i);
+
+		for (int j = 0; j < numToPushTo; j++) {
+			if (numCard.contains(i + j + 1)) {
+				numCard[i + j + 1] += sourceCards;
+			}
+		}
+	}
 
 	auto end = std::chrono::high_resolution_clock::now();
 	auto dur = end - start;
 
 
 	fmt::print("Processed 1: {}\n", totalScore);
-	//fmt::print("Processed 2: {}\n", gearRatioSum);
+	fmt::print("Processed 2: {}\n", numCardsProcessed);
 
 
 	fmt::print("Took {}\n", dur);
