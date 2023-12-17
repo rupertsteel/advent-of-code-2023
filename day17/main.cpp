226a92ff97d8873d23bbbcfba0056620baab62bf
Rupert Steel <rupert@rupertsteel.me>
2023-12-17 21:28:23 +1300

    Day 17 part 2

--- day17/main.cpp
+++ day17/main.cpp
@@ -77,6 +77,8 @@ struct Path {
 	std::strong_ordering operator<=>(const Path& other) const {
 		return (currentCost + estMinFutureCost) <=> (other.currentCost + other.estMinFutureCost);
 	}
+
+	std::vector<Point> tracePoints;
 };
 
 // thinking about scores, we have a lower bound (assuming 1 cost to the end, and a upper bound assuming 9 cost to the end)
@@ -231,6 +233,179 @@ int aStarSearch(const Block& map, Point startingPos, Point endPos) {
 	return bestScore;
 }
 
+void debugPrintMap(const Block& map, Path& path) {
+	std::string tmpStr;
+
+	for (int y = 0; y < map.height; y++) {
+		for (int x = 0; x < map.width; x++) {
+			bool printedPathVal = false;
+
+			for (auto& point : path.tracePoints) {
+				if (point.x == x && point.y == y) {
+					tmpStr.push_back('*');
+					printedPathVal = true;
+				}
+			}
+
+			if (!printedPathVal) {
+				tmpStr.push_back(map.map.at(Point{ x, y }) + '0');
+			}
+		}
+
+		tmpStr.push_back('\n');
+	}
+
+	fmt::print("{}\n", tmpStr);
+}
+
+int aStarSearchV2(const Block& map, Point startingPos, Point endPos) {
+
+	std::map<Beam, int> exploredScores;
+
+	std::priority_queue<Path, std::vector<Path>, std::greater<>> queue;
+
+	queue.push(Path{ {startingPos, {1, 0}, 10}, 0, manhattanDistance(startingPos, endPos) });
+	queue.push(Path{ {startingPos, {0, 1}, 10}, 0, manhattanDistance(startingPos, endPos) });
+
+	bool foundEnd = false;
+
+	int bestScore = std::numeric_limits<int>::max();
+	std::optional<Path> bestPath;
+
+	uint64_t processed = 0;
+
+	auto mapTotalExplorePoints = map.width * map.height * 4 * 10;
+	auto collapseThreshold = mapTotalExplorePoints * 2;
+
+	while (!queue.empty() && (!foundEnd || (queue.top().currentCost + queue.top().estMinFutureCost - 10) < bestScore)) {
+		if (queue.size() > collapseThreshold) {
+			auto sizeBefore = queue.size();
+			std::map<Beam, Path> collapsedPaths;
+
+			while (!queue.empty()) {
+				auto val = queue.top();
+				queue.pop();
+
+				if (!collapsedPaths.contains(val.beam)) {
+					collapsedPaths[val.beam] = val;
+				} else {
+					if (collapsedPaths[val.beam].currentCost > val.currentCost) {
+						collapsedPaths[val.beam] = val;
+					}
+				}
+			}
+
+			for (auto& [key, value] : collapsedPaths) {
+				queue.push(value);
+			}
+
+			auto sizeAfter = queue.size();
+
+			fmt::println("Collapse, before: {}, after: {}, diff: {}", sizeBefore, sizeAfter, sizeBefore - sizeAfter);
+
+			continue;
+		}
+
+		auto nextPath = queue.top();
+		queue.pop();
+
+		processed++;
+
+		if ((processed & 0xFFFFFF) == 0) {
+			auto mapExploreFraction = static_cast<float>(exploredScores.size()) / mapTotalExplorePoints;
+			fmt::println("{} iterations, map is {}% explored, {} items in the queue, score {}/{}", processed, mapExploreFraction * 100.0f, queue.size(), nextPath.currentCost, nextPath.currentCost + nextPath.estMinFutureCost);
+		}
+
+		if (exploredScores.contains(nextPath.beam) && exploredScores.at(nextPath.beam) < (nextPath.currentCost + nextPath.estMinFutureCost)) {
+			continue;
+		}
+
+		exploredScores[nextPath.beam] = (nextPath.currentCost + nextPath.estMinFutureCost);
+
+		auto newPos = nextPath.beam.currentPos + nextPath.beam.directionChange;
+		if (newPos.x < 0 || newPos.y < 0 || newPos.x >= map.width || newPos.y >= map.height) {
+			// skip out of bounds
+			continue;
+		}
+
+		// add the movement cost
+		int newCurrentCost = nextPath.currentCost + map.map.at(newPos);
+		int newEstMinFutureCost = manhattanDistance(newPos, endPos);
+
+		if (newPos == endPos && nextPath.beam.spacesLeft <= 7) {
+			foundEnd = true;
+			if (newCurrentCost < bestScore) {
+				bestScore = newCurrentCost;
+				bestPath = nextPath;
+			}
+			continue;
+		}
+
+		// if we can move forward, do so
+		if (nextPath.beam.spacesLeft > 1) {
+			auto newPath = nextPath;
+			newPath.beam = Beam{ newPos, nextPath.beam.directionChange, nextPath.beam.spacesLeft - 1 };
+			newPath.currentCost = newCurrentCost;
+			newPath.estMinFutureCost = newEstMinFutureCost;
+			newPath.tracePoints.push_back(newPos);
+			queue.push(newPath);
+		}
+
+		auto checkNextPathValid = [&](const Path& p) {
+			auto nextPos = p.beam.currentPos + p.beam.directionChange;
+			if (nextPos.x < 0 || nextPos.y < 0 || nextPos.x >= map.width || nextPos.y >= map.height) {
+				return false;
+			}
+
+			if (exploredScores.contains(p.beam) && exploredScores.at(p.beam) < (p.currentCost + p.estMinFutureCost)) {
+				return false;
+			}
+
+			return true;
+		};
+
+		Path newPath = nextPath;
+		newPath.currentCost = newCurrentCost;
+		newPath.estMinFutureCost = newEstMinFutureCost;
+		newPath.tracePoints.push_back(newPos);
+
+		// add in our turns if we can
+		if (nextPath.beam.spacesLeft <= 7) {
+			if (nextPath.beam.directionChange.x == 0) {
+				{
+					newPath.beam = Beam{ newPos, {1, 0}, 10 };
+					if (checkNextPathValid(newPath)) {
+						queue.push(newPath);
+					}
+				}
+				{
+					newPath.beam = Beam{ newPos, {-1, 0}, 10 };
+					if (checkNextPathValid(newPath)) {
+						queue.push(newPath);
+					}
+				}
+			} else {
+				{
+					newPath.beam = Beam{ newPos, {0, 1}, 10 };
+					if (checkNextPathValid(newPath)) {
+						queue.push(newPath);
+					}
+				}
+				{
+					newPath.beam = Beam{ newPos, {0, -1}, 10 };
+					if (checkNextPathValid(newPath)) {
+						queue.push(newPath);
+					}
+				}
+			}
+		}
+	}
+
+	debugPrintMap(map, *bestPath);
+
+	return bestScore;
+}
+
 int main(int argc, char* argv[]) {
 	std::ifstream inputFile("inputs/day17.txt");
 	std::string input(std::istreambuf_iterator{ inputFile }, std::istreambuf_iterator<char>{});
@@ -265,7 +440,8 @@ int main(int argc, char* argv[]) {
 
 	auto map = maps[0];
 
-	auto bestScore = aStarSearch(map, { 0, 0 }, { map.width - 1, map.height - 1 });
+	//auto bestScore = aStarSearch(map, { 0, 0 }, { map.width - 1, map.height - 1 });
+	auto bestScore = aStarSearchV2(map, { 0, 0 }, { map.width - 1, map.height - 1 });
 
 	auto end = std::chrono::high_resolution_clock::now();
 	auto dur = end - start;
